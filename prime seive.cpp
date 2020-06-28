c2e9d2f2a5aafe50623e237d9b0007c9e83d577d
Chandra-sinha1999 <54039460+Chandra-sinha1999@users.noreply.github.com>
2020-06-28 16:20:40 +0530

    Create prime seive.cpp

---
+++ prime seive.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void prime(int *p){
+    for(ll i = 3; i < 1000000; i+=2) p[i] = 1;
+        for(ll i = 3;i < 1000000; i+=2){
+            if(p[i] == 1){
+                for(ll j = i * i; j <= 1000000; j += i){
+                    p[j] = 0;
+                }
+            }
+    }
+    p[2] = 1;
+}
+int main() {
+	int n;
+	cin >> n;
+	int p[1000000] = {0};
+	prime(p);
+	for(int i = 2; i <= n; i++){
+	    if(p[i]) cout <<i<<" ";
+	}
+	cout << endl;
+	return 0;
+}
